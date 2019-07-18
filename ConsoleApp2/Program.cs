15497b30f3474d43c4cb6b4e0c6f2c925b6d91b1
BoyunetsDV <boyunets.dv@gmail.com>
2019-07-18 11:04:34 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine(Fold(arr, (a, b) => a + b));
+            Console.WriteLine(Fold(arr, (a, b) => a * b));
+
+            Console.WriteLine(Fold(arr, (a, b) => a + b, 10));
+            Console.WriteLine(Fold(arr, (a, b) => a * b, 10));
+
+            Console.WriteLine(Fold(arr, (a, b) => a + b.ToString(),""));
+
+            Console.ReadLine();
+        }
+
+        public static T Fold<T>(T[] arr, Func<T, T, T> func)
+        {
+            return Fold<T, T>(arr, func, arr[0]);
+        }
+        public static T2 Fold<T1,T2>(T1[] arr, Func<T2, T1, T2> func, T2 res)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                res = func(res, arr[i]);
+            return res;
+        }
+
+    }
+}
