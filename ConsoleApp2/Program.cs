17b869db52ee507c90b91e2e08867966c8cb1fb6
BoyunetsDV <boyunets.dv@gmail.com>
2019-08-01 13:57:40 +0300

    update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,48 +6,46 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 2, 2, 3, 4 };
-            Console.WriteLine(Fold(arr, (a, b) => a + b));
-            Console.WriteLine(Fold(arr, (a, b) => a * b));
+            FoldSomething(new int[] { 2, 2, 3, 4 }, 10);
+            FoldSomething(new double[] { 2.5, 2, 3, 4 }, 10);
+            FoldSomething(new decimal[] { 2, 2.9m, 3.3m, 4 }, 10);
 
-            Console.WriteLine(Fold(arr, (a, b) => a + b, 0, 10));
-            Console.WriteLine(Fold(arr, (a, b) => a * b, 0, 10));
+            Console.ReadLine();
+        }
+
+        private static void FoldSomething<T>(T[] arr, T startNumber)
+        {
+            Console.WriteLine(Fold(arr, (a, b) => AddValues(a, b)));
+            Console.WriteLine(Fold(arr, (a, b) => MultiplyValues(a, b)));
+            Console.WriteLine(Fold(arr, (a, b) => AddValues(a, b), 0, startNumber));
+            Console.WriteLine(Fold(arr, (a, b) => MultiplyValues(a, b), 0, startNumber));
 
             Console.WriteLine(Fold(arr, (a, b) => a + b.ToString(), 0, ""));
             Console.WriteLine();
-
-            double[] arr2 = new double[] { 2.5, 2, 3, 4 };
-            Console.WriteLine(Fold(arr2, (a, b) => a + b));
-            Console.WriteLine(Fold(arr2, (a, b) => a * b));
-                                      
-            Console.WriteLine(Fold(arr2, (a, b) => a + b, 0, 10d));
-            Console.WriteLine(Fold(arr2, (a, b) => a * b, 0, 10d));
-                                      
-            Console.WriteLine(Fold(arr2, (a, b) => a + b.ToString(), 0, ""));
-            Console.WriteLine();
-
-            decimal[] arr3 = new decimal[] { 2, 2.9m, 3.3m, 4 };
-            Console.WriteLine(Fold(arr3, (a, b) => a + b));
-            Console.WriteLine(Fold(arr3, (a, b) => a * b));
-                                      
-            Console.WriteLine(Fold(arr3, (a, b) => a + b, 0, 10m));
-            Console.WriteLine(Fold(arr3, (a, b) => a * b, 0, 10m));
-                                      
-            Console.WriteLine(Fold(arr3, (a, b) => a + b.ToString(), 0, ""));
-            Console.WriteLine();
-
-            Console.ReadLine();
         }
 
         public static T Fold<T>(T[] arr, Func<T, T, T> func)
         {
             return Fold(arr, func, 1, arr[0]);
         }
-        public static T2 Fold<T1, T2>(T1[] arr, Func<T2, T1, T2> func, int startPosition, T2 res)
+        public static T2 Fold<T1, T2>(T1[] arr, Func<T2, T1, T2> func, int startPosition, T2 result)
         {
             for (int i = startPosition; i < arr.Length; i++)
-                res = func(res, arr[i]);
-            return res;
+                result = func(result, arr[i]);
+            return result;
+        }
+
+        public static T1 AddValues<T1, T2>(T1 x, T2 y)
+        {
+            dynamic tx = x;
+            dynamic ty = y;
+            return tx + ty;
+        }
+        public static T1 MultiplyValues<T1, T2>(T1 x, T2 y)
+        {
+            dynamic tx = x;
+            dynamic ty = y;
+            return tx * ty;
         }
 
     }
